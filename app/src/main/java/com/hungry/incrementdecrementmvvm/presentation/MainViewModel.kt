8be5095bda9b582cbf6d9a41a6429c3f938f1816
Dian Permata Kusuma <diankusuma664@gmail.com>
2023-09-27 15:29:24 +0700

    no-patterns update

--- app/src/main/java/com/hungry/incrementdecrementmvvm/presentation/MainViewModel.kt
+++ app/src/main/java/com/hungry/incrementdecrementmvvm/presentation/MainViewModel.kt
@@ -5,21 +5,21 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-
-    private val _counter : MutableLiveData<Int> = MutableLiveData<Int>().apply {
+    private val _counter: MutableLiveData<Int> = MutableLiveData<Int>().apply {
         postValue(0)
     }
-    val counter : LiveData<Int>
+    val counter: LiveData<Int>
         get() = _counter
 
-    fun increment(){
+    fun increment() {
         val currentValue = _counter.value ?: 0
         val value = currentValue + 1
         _counter.postValue(value)
     }
 
-    fun decrement(){
+    fun decrement() {
         val currentValue = _counter.value ?: 0
+        if (currentValue <= 0) return
         val value = currentValue - 1
         _counter.postValue(value)
     }
