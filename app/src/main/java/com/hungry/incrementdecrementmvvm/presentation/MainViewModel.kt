5eed07aaf559fcea1293b2f902ebedb796c6d26c
Dian Permata Kusuma <diankusuma664@gmail.com>
2023-09-27 14:43:49 +0700

    Initial commit

---
+++ app/src/main/java/com/hungry/incrementdecrementmvvm/presentation/MainViewModel.kt
@@ -0,0 +1,26 @@
+package com.hungry.incrementdecrementmvvm.presentation
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    private val _counter : MutableLiveData<Int> = MutableLiveData<Int>().apply {
+        postValue(0)
+    }
+    val counter : LiveData<Int>
+        get() = _counter
+
+    fun increment(){
+        val currentValue = _counter.value ?: 0
+        val value = currentValue + 1
+        _counter.postValue(value)
+    }
+
+    fun decrement(){
+        val currentValue = _counter.value ?: 0
+        val value = currentValue - 1
+        _counter.postValue(value)
+    }
+}
